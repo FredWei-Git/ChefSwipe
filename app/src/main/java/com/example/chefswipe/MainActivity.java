9ea32e8673ebd4fbd42084f31d4dadceb742a749
FredWei-Hub <fredwei2001@gmail.com>
2021-02-05 21:46:23 -0500

    refresh

--- app/src/main/java/com/example/chefswipe/MainActivity.java
+++ app/src/main/java/com/example/chefswipe/MainActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
