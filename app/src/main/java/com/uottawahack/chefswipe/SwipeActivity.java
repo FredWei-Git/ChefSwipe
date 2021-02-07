defb28d1416972d642b669a8c663efe7e845cdff
david-huynh <huynh.david.work@gmail.com>
2021-02-06 20:24:09 -0500

    Reformatted SwipeActivity

--- app/src/main/java/com/uottawahack/chefswipe/SwipeActivity.java
+++ app/src/main/java/com/uottawahack/chefswipe/SwipeActivity.java
@@ -2,40 +2,47 @@ package com.uottawahack.chefswipe;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
 import androidx.customview.widget.ViewDragHelper;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.AttributeSet;
+import android.view.DragEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.material.card.MaterialCardView;
 
 
-public class SwipeActivity extends AppCompatActivity {
+public class SwipeActivity extends AppCompatActivity implements View.OnClickListener {
     private SwipeViewModel vm;
+    private MaterialCardView card;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
+        findViewById(R.id.settingsButton).setOnClickListener(this);
+
+        card = findViewById(R.id.recipeCardView);
         vm = new ViewModelProvider(this).get(SwipeViewModel.class);
-        //Update UI when data is changed
         vm.makeSwipeRequest("chicken", "alcohol-free");
+        //Update UI when data is changed
 
+        /*vm.getRecipe().observe(this, RecipeInfo ->{
+
+        });*/
     }
-    private class ViewDragHelperCallback extends ViewDragHelper.Callback{
-        @Override
-        public void onViewCaptured(@NonNull View capturedChild, int activePointerId) {
-            super.onViewCaptured(capturedChild, activePointerId);
-        }
 
-        @Override
-        public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
-            super.onViewReleased(releasedChild, xvel, yvel);
-        }
-
-        @Override
-        public boolean tryCaptureView(@NonNull View child, int pointerId) {
-            return false;
-        }
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(intent);
     }
+
+
+
 }
